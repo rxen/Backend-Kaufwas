9a9556f43406f6868f36c7d17d2192c7d78bd616
rxen <57657706+rxen@users.noreply.github.com>
2019-12-11 16:17:26 +0100

    well well

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(bodyParser.json())
-app.route('/api/cats').post((req, res) => {
-  res.send(201, req.body)
-})
+// app.route('/api/cats').post((req, res) => {
+//   res.send(201, req.body)
+// })
 
 var corsOptions = {
     origin: 'http://localhost:4200',
@@ -36,44 +36,30 @@ app.listen(3000, () =>{
 });
 
 
-// app.route('/api/cats').get((req, res) => {
-//     res.send({
-//       cats: [{ name: 'lilly' }, { name: 'lucy' }],
-//     });
-//   });
 
-//   app.route('/api/cats/:name').get((req, res) => {
-//     const requestedCatName = req.params['name']
-//     res.send({ name: requestedCatName })
-//   })
 
-app.route('/').get((req,res) => {
+app.get('/', (req,res) => {
     res.json('Server Ready');
 });
 
 
-// app.head("/simple-cors", cors(), (req, res) => {
-//     console.info("HEAD /simple-cors");
-//     res.sendStatus(204);
-//   });
-//   app.get("/simple-cors", cors(), (req, res) => {
-//     console.info("GET /simple-cors");
-//     res.json({
-//       text: "Simple CORS requests are working. [GET]"
-//     });
 
-// eingabe von URL, : ist platzhalter für verschiedene Ausprägungen von type
+
+
+// // eingabe von URL, : ist platzhalter für verschiedene Ausprägungen von type
 app.get('/products/:type', (req,res)=>
 {
+
     var connection = mysql.createConnection({
-            host     : 'remotemysql.com',
-            user     : 'QEaqoXymWu',
-            password: 'tT81u7BeH8',
-            database : 'QEaqoXymWu'
-          
-        });
+        host     : 'remotemysql.com',
+        user     : 'QEaqoXymWu',
+        password: 'tT81u7BeH8',
+        database : 'QEaqoXymWu'
+      
+    });
 
     connection.connect((err)=>{
+        
         if(err) 
             {throw err;}   
             //? ist Platzhalter, [ req.params.type ] befühlt ? 
@@ -93,4 +79,83 @@ app.get('/products/:type', (req,res)=>
    
 });
 
+app.get('/products', (req,res)=>
+{
 
+    var connection = mysql.createConnection({
+        host     : 'remotemysql.com',
+        user     : 'QEaqoXymWu',
+        password: 'tT81u7BeH8',
+        database : 'QEaqoXymWu'
+      
+    });
+
+    connection.connect((err)=>{
+        
+        if(err) 
+            {throw err;}   
+            //? ist Platzhalter, [ req.params.type ] befühlt ? 
+            //durch Platzhalter werden SQL injection Angriffe verhindert
+             
+            connection.query('Select * from items ', (err,result) => {
+                if(err) 
+                    {throw err;}
+        
+                //zugriff darauf(wie return)
+                return res.json(result);
+            });
+        
+            connection.end();
+            
+    });
+   
+});
+
+app.get('/bestellung', (req,res)=>
+{
+
+    res.send('fuckq')
+
+});
+
+// app.post('/bestellung',(req,res)=>
+// {
+
+
+//     var connection = mysql.createConnection({
+//         host     : 'remotemysql.com',
+//         user     : 'QEaqoXymWu',
+//         password: 'tT81u7BeH8',
+//         database : 'QEaqoXymWu'
+      
+//     });
+//     connection.connect((err)=>{
+
+
+//         if(err) 
+//             {throw err;}   
+//             //? ist Platzhalter, [ req.params.type ] befühlt ? 
+//             //durch Platzhalter werden SQL injection Angriffe verhindert
+            
+//             var values = [
+//                 ['John', 'abe road'],
+//                 ['fick', 'dich'],
+//                 ['du', 'scheiß'],
+//                 ['programm','!']
+//             ];
+
+//             connection.query('INSERT INTO test(name, adress) SET ?', [values], (err,res) => {
+//                 if(err) 
+//                     {throw err;}
+
+//                     console.log('scheiße')
+        
+//                                     //zugriff darauf(wie return)
+                                    
+//             });
+        
+//             connection.end();
+            
+//     });
+   
+// });
